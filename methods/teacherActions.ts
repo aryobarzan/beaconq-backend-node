3130076b8d113e0f7f70267be93ec40840d00262
aryobarzan <aryobarzan.atashpendar@gmail.com>
2025-11-22 11:35:41 +0100

    deprecated teacherActions

---
+++ methods/teacherActions.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+
+///
+const functions = {
+  // DEPRECATED: previously, this endpoint would allow the teacher to delete their ActivityUserAnswers and ScheduledQuizUserStarts.
+  // For backwards compatibility, this endpoint still exists but does nothing.
+  resetTeacherAnswers: function (req: Request, res: Response) {
+    return res
+      .status(500)
+      .send({ message: "Answer reset failed: an error occurred." });
+  }
+};
+
+export default functions;
